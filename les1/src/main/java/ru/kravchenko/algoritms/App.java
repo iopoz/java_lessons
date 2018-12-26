500d56ecfb2ca187ca82104357c0d8b6ef9c9d05
iopoz <2909kev@gmail.com>
2018-12-26 17:01:31 +0300

    first lesson - empty project

--- les1/src/main/java/ru/kravchenko/algoritms/App.java
+++ les1/src/main/java/ru/kravchenko/algoritms/App.java
@@ -1,5 +1,8 @@
 package ru.kravchenko.algoritms;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  *
  *
@@ -8,6 +11,42 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        final int sizeOfArray = 1000000;
+        int[] createdArray = createRandomArray(sizeOfArray);
+        final Random rand = new Random();
+        final int newNum = rand.nextInt(sizeOfArray + 1);
+
+        System.out.println( "Created Array" );
+        printArray(createdArray);
+
+        System.out.println("Added element to end of Array: " + newNum);
+        printArray(addElementToArray(createdArray, newNum));
+
+
     }
+
+    public static int[] createRandomArray(int arraySize){
+        int[] bigArray = new int[arraySize];
+        Random rand = new Random();
+        for (int i=0; i < bigArray.length; i++) {
+            bigArray[i] = rand.nextInt(arraySize);
+        }
+        return bigArray;
+    }
+
+    public static int[] addElementToArray(int[] array, int num){
+        int[] result = Arrays.copyOf(array, array.length + 1);
+        result[result.length - 1] = num;
+        return result;
+    }
+
+    public static void printArray(int[] array){
+        for (int aPrintedArray : array) {
+            System.out.println(aPrintedArray);
+        }
+    }
+
+//    public static int findElementByIndex(int[] array, int index){
+//
+//    }
 }
