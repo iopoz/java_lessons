b5cdf6ff97e358d08500dc329133ea1b69c9a48f
iopoz <2909kev@gmail.com>
2018-12-28 11:32:45 +0300

    homework 2

---
+++ les1/src/main/java/ru/kravchenko/algoritms/homework_2/Array.java
@@ -0,0 +1,31 @@
+package ru.kravchenko.algoritms.homework_2;
+
+public interface Array {
+    void add(int value);
+
+    int get();
+
+    int get(int index);
+
+    int indexOf(int value);
+
+    boolean contains(int value);
+
+    boolean remove(int value);
+
+    void remove();
+
+    void clear();
+
+    boolean isEmpty();
+
+    int getSize();
+
+    void display();
+
+    void sortBubble();
+
+    void sortSelect();
+
+    void sortInsert();
+}
