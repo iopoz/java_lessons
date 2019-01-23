a9c3a0dd371205624530641c49d8a14234df96d8
iopoz <2909kev@gmail.com>
2019-01-23 13:14:48 +0300

    homework 6 - done

---
+++ les1/src/main/java/ru/kravchenko/algoritms/homework_6/Tree.java
@@ -0,0 +1,19 @@
+package ru.kravchenko.algoritms.homework_6;
+
+public interface Tree {
+    void add(int value);
+
+    boolean remove(int value);
+
+    boolean find(int value);
+
+    public int getSize();
+
+    boolean isEmpty();
+
+    void traverse(TraverseMode mode);
+
+    void displayTree();
+
+    boolean isBalanced();
+}
